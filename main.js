255de341c99b4955f0c3e7607e851b3b1ae9d204
MariaIngerslev <235301725+MariaIngerslev@users.noreply.github.com>
2025-11-18 11:06:43 +0100

    feat: add if-else statements for computer choice

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ function getComputerChoice () {
     let randomNumber = Math.random();
     let randomChoice = Math.floor(randomNumber * 3);
 
-    IF randomChoice = 0 THEN
-        RETURN "rock"
-    ELSE IF randomChoice = 1 THEN
-        RETURN "paper"
-    ELSE
-        RETURN "scissors"
-    END IF 
-}
+    if (randomChoice === 0) {
+        return "rock";
+    } else if (randomChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
