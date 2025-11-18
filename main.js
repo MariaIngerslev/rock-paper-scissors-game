dd33f5ba3509788e6e5b57a17ea767a0b8312f63
MariaIngerslev <235301725+MariaIngerslev@users.noreply.github.com>
2025-11-18 10:36:07 +0100

    feat: implement pseudocode for computer's random choice logic

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+FUNCTION getComputerChoice
+    SET randomNumber TO random number between 0 and 1
+    SET randomChoice TO FLOOR(randomNumber * 3)
+
+    IF randomChoice = 0 THEN
+        RETURN "rock"
+    ELSE IF randomChoice = 1 THEN
+        RETURN "paper"
+    ELSE
+        RETURN "scissors"
+    END IF 
+    
+END FUNCTION
