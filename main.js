65682da454e5806fe2fa25b52c92f962c6fef8ba
MariaIngerslev <235301725+MariaIngerslev@users.noreply.github.com>
2025-11-20 09:45:28 +0100

    fix: clean up indentation and formatting in getHumanChoice and score variables

--- main.js
+++ main.js
@@ -12,49 +12,45 @@ function getComputerChoice () {
 }
 
 function getHumanChoice() {
-
-let usersInput = prompt("Enter rock, paper, or scissors");
-return usersInput
+    let usersInput = prompt("Enter rock, paper, or scissors");
+    return usersInput;
 }
 
-let humanScore = 0
-let computerScore = 0
+let humanScore = 0;
+let computerScore = 0;
 
 FUNCTION playRound(humanChoice, computerChoice)
-    humanChoice = getHumanChoice()
-    humanChoice = LOWERCASE(humanChoice)
 
-    computerChoice = getComputerChoice()
+    SET humanChoice TO LOWERCASE(getHumanChoice())
+    SET computerChoice TO getComputerChoice()
 
-IF humanChoice EQUALS computerChoice THEN
-    PRINT "It's a tie! Both chose " + humanChoice
+    IF humanChoice EQUALS computerChoice THEN
+        PRINT "It's a tie! Both chose " + humanChoice
 
-ELSE 
-IF humanChoice EQUALS "rock" THEN
-    IF computerChoice EQUALS "scissors" THEN
-    PRINT "You win! Rock beats Scissors"
-    INCREMENT humanScore
+    ELSE IF humanChoice EQUALS "rock" THEN
+        IF computerChoice EQUALS "scissors" THEN
+            PRINT "You win! Rock beats Scissors"
+            INCREMENT humanScore
+        ELSE
+            PRINT "You lose! Paper beats Rock"
+            INCREMENT computerScore
+
+    ElSE IF humanChoice EQUALS "paper" THEN
+        IF computerChoice EQUALS "rock" THEN
+            PRINT "You win! Paper beats Rock"
+            INCREMENT humanScore
+        ELSE
+            PRINT "You lose! Scissors beats Paper"
+            INCREMENT computerScore
+
+    ELSE IF humanChoice EQUALS "scissors" THEN
+        IF computerChoice EQUALS "paper" THEN
+            PRINT "You win! Scissors beats Paper"
+            INCREMENT humanScore
+        ELSE
+            PRINT "You lose! Rock beats Scissors"
+            INCREMENT computerScore
     ELSE
-    PRINT "You lose! Paper beats Rock"
-    INCREMENT computerScore
-ElSE
-IF humanChoice EQUALS "paper" THEN
-    IF computerChoice EQUALS "rock" THEN
-    PRINT "You win! Paper beats Rock"
-    INCREMENT humanScore
-    ELSE
-    PRINT "You lose! Scissors beats Paper"
-    INCREMENT computerScore
-
-ELSE
-IF humanChoice EQUALS "scissors" THEN
-    IF computerChoice EQUALS "paper" THEN
-    PRINT "You win! Scissors beats Paper"
-    INCREMENT humanScore
-    ELSE
-    PRINT "You lose! Rock beats Scissors"
-    INCREMENT computerScore
-ELSE
-    PRINT "Invalid choice! Please choose rock, paper, or scissors."
-
-    END FUNCTION
+        PRINT "Invalid choice! Please choose rock, paper, or scissors."
+    END IF
+END FUNCTION
