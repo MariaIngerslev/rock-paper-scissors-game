abe2e2f159cac07d3d213138ab16e9a363f44125
Maria Ingerslev <235301725+MariaIngerslev@users.noreply.github.com>
2025-12-10 10:34:19 +0100

    refactor: simplify getComputerChoice using CHOICES array lookup

--- main.js
+++ main.js
@@ -24,17 +24,9 @@ let roundNumber = 0;
 let gameOver = false;
 
 // --- COMPUTER CHOICE --- //
-function getComputerChoice () {
-    let randomNumber = Math.random();
-    let randomChoice = Math.floor(randomNumber * 3);
-
-    if (randomChoice === 0) {
-        return "rock";
-    } else if (randomChoice === 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+function getComputerChoice() {
+  const index = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[index];
 }
 
 // --- PLAY ONE ROUND --- //
